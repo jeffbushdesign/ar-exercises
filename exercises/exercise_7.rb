72ff01397e884f2605defc509f3e559b459d626a
Jeff Bush <jeffbushdesign@gmail.com>
2022-07-03 16:06:55 -0400

    Complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -5,8 +5,14 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
-
 puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name:"
+new_store = gets.chomp
+
+new_store1 = Store.create(name: new_store)
+errs = new_store1.errors.full_messages
+# if newly created store is not valid, output error messages
+errs.each { |e| puts e } if !new_store1.valid?
